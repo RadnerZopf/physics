cd08e57063624ad0a5fa43cd9ec88b9f5a0c3097
florian zopf <florian.zopf@gmail.com>
2017-12-14 20:17:59 +0100

    added new files from moodle + copy pasted sphereSystem cpp and header

---
+++ Simulations/SphereSystem.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include "util/vectorbase.h"
+#include "util/matrixbase.h"
+#include "DrawingUtilitiesClass.h"
+#include "collisionDetect.h"
+
+using std::vector; 
+
+class RigidBodySystem
+{
+
+
+public:
+	 RigidBodySystem(Vec3 _position,Vec3 _size,float _mass, Quat _orientation, Vec3 _linearVelocity, Vec3 _angularVelocity, float _bounciness);
+	~RigidBodySystem();
+
+	void applyForce(Vec3 _loc, Vec3 _force);
+
+
+	void saveState();
+	void loadOldState();
+
+	Mat4 getInertiaTensor(); 
+
+
+	Mat4& getWorldMat(); 
+
+
+	Vec3 position;
+	Vec3 size; 
+	Quat orientation; 
+	Vec3 linearVelocity;
+	Vec3 angularVelocity; 
+	Vec3 angularMomentum = Vec3();
+
+	Vec3 old_position;
+	Vec3 old_size;
+	Quat old_orientation;
+	Vec3 old_linearVelocity;
+	Vec3 old_angularVelocity;
+	Vec3 old_angularMomentum = Vec3();
+
+
+
+
+	Vec3 force = Vec3(), torque = Vec3(); 
+	float mass; 
+	float bouncieness; 
+	bool fixed = false;
+
+private: 
+
+	Mat4 inertiaTensor;
+
+	GamePhysics::Mat4 worldMat; 
+	GamePhysics::Mat4 scaleMat;
+	vector<Vec3> points;
+
+};
+
