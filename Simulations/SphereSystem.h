449d37f5b14e98f680cee9d83f0c13ac742bbcf9
Patrick Radner <radner.patrick@tum.de>
2017-12-14 21:27:08 +0100

    basic stuff, everything bugged
    # Conflicts:
    #	Simulations/SphereSystem.cpp
    #	Simulations/SphereSystem.h
    #	Simulations/SphereSystemSimulator.cpp
    #	Simulations/SphereSystemSimulator.h

--- Simulations/SphereSystem.h
+++ Simulations/SphereSystem.h
@@ -1,6 +1,5 @@
 #pragma once
 
-<<<<<<< HEAD
 #include <vector>
 #include "util\vectorbase.h"
 
@@ -25,68 +24,6 @@ public:
 	~SphereSystem();
 
 	std::vector<Sphere> spheres;
-=======
-#define _USE_MATH_DEFINES
-#include <cmath>
-#include <iostream>
-#include <vector>
-#include "util/vectorbase.h"
-#include "util/matrixbase.h"
-#include "DrawingUtilitiesClass.h"
-#include "collisionDetect.h"
-
-using std::vector; 
-
-class RigidBodySystem
-{
-
-
-public:
-	 RigidBodySystem(Vec3 _position,Vec3 _size,float _mass, Quat _orientation, Vec3 _linearVelocity, Vec3 _angularVelocity, float _bounciness);
-	~RigidBodySystem();
-
-	void applyForce(Vec3 _loc, Vec3 _force);
-
-
-	void saveState();
-	void loadOldState();
-
-	Mat4 getInertiaTensor(); 
-
-
-	Mat4& getWorldMat(); 
-
-
-	Vec3 position;
-	Vec3 size; 
-	Quat orientation; 
-	Vec3 linearVelocity;
-	Vec3 angularVelocity; 
-	Vec3 angularMomentum = Vec3();
-
-	Vec3 old_position;
-	Vec3 old_size;
-	Quat old_orientation;
-	Vec3 old_linearVelocity;
-	Vec3 old_angularVelocity;
-	Vec3 old_angularMomentum = Vec3();
-
-
-
-
-	Vec3 force = Vec3(), torque = Vec3(); 
-	float mass; 
-	float bouncieness; 
-	bool fixed = false;
-
-private: 
-
-	Mat4 inertiaTensor;
-
-	GamePhysics::Mat4 worldMat; 
-	GamePhysics::Mat4 scaleMat;
-	vector<Vec3> points;
->>>>>>> cd08e57063624ad0a5fa43cd9ec88b9f5a0c3097
 
 };
 
--- Simulations/SphereSystemSimulator.h
+++ Simulations/SphereSystemSimulator.h
@@ -1,15 +1,11 @@
 #ifndef SPHSYSTEMSIMULATOR_h
 #define SPHSYSTEMSIMULATOR_h
 #include "Simulator.h"
-<<<<<<< HEAD
 #include "SphereSystem.h"
-=======
->>>>>>> cd08e57063624ad0a5fa43cd9ec88b9f5a0c3097
 //#include "spheresystem.h", add your sphere system header file
 
 #define NAIVEACC 0
 #define GRIDACC 1
-<<<<<<< HEAD
 #define KDTREEACC 2
 
 #define EARTH_ACCEL GamePhysics::Real(-9.81)
@@ -20,8 +16,6 @@ struct Box
 	Vec3 pos; 
 	Vec3 posPlusoffset; 
 };
-=======
->>>>>>> cd08e57063624ad0a5fa43cd9ec88b9f5a0c3097
 
 class SphereSystemSimulator:public Simulator{
 public:
@@ -39,7 +33,6 @@ public:
 	void onMouse(int x, int y);
 	
 protected:
-<<<<<<< HEAD
 
 
 	inline void simNaiveAcc(float dt); 
@@ -50,8 +43,6 @@ protected:
 	inline void detectAndResolveSphereOnBoundsCollision(SphereSystem* system, int first,  float dt);
 
 
-=======
->>>>>>> cd08e57063624ad0a5fa43cd9ec88b9f5a0c3097
 	// Attributes
 	Vec3 externalForce;
 	Point2D m_mouse;
@@ -62,7 +53,6 @@ protected:
 	float m_fForceScaling;
 	float m_fDamping;
 	int   m_iNumSpheres;
-<<<<<<< HEAD
 
 	float m_fRadTimes2;
 	float m_fRadTimes2Squared; //squared dist
@@ -70,27 +60,17 @@ protected:
 	Box m_boxOuterBounds; 
 	Box m_boxRadiusBounds;
 
-=======
->>>>>>> cd08e57063624ad0a5fa43cd9ec88b9f5a0c3097
 	
 	int   m_iKernel; // index of the m_Kernels[5], more detials in SphereSystemSimulator.cpp
 	static std::function<float(float)> m_Kernels[5];
 	
 	int   m_iAccelerator; // switch between NAIVEACC and GRIDACC, (optionally, KDTREEACC, 2)
 	
-<<<<<<< HEAD
 	SphereSystem * m_pSphereSystem = nullptr; // add your own sphere system member!
 	// for Demo 3 only:
 	// you will need multiple SphereSystem objects to do comparisons in Demo 3
 	// m_iAccelerator should be ignored.
 	SphereSystem * m_pSphereSystemGrid = nullptr;
-=======
-	//SphereSystem * m_pSphereSystem; // add your own sphere system member!
-	// for Demo 3 only:
-	// you will need multiple SphereSystem objects to do comparisons in Demo 3
-	// m_iAccelerator should be ignored.
-	// SphereSystem * m_pSphereSystemGrid; 
->>>>>>> cd08e57063624ad0a5fa43cd9ec88b9f5a0c3097
 
 };
 
